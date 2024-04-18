407b9f0cd6c72f03912efbef2e17ae5301441209
TorarinT <watcher.torarin@gmail.com>
2024-04-18 02:11:48 +0200

    Full lecture code

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TransitMapApplication } from "./transitMapApplication";
+import { TransitMapApplication } from "./components/app/transitMapApplication";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(<TransitMapApplication />);
